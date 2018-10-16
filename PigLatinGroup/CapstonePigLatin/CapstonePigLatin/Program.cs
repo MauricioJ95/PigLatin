f05c01d9d84f696a91ed0f7ee02aa6f09f839b7e
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-15 23:23:39 -0400

    added comments

--- PigLatinGroup/CapstonePigLatin/CapstonePigLatin/Program.cs
+++ PigLatinGroup/CapstonePigLatin/CapstonePigLatin/Program.cs
@@ -15,43 +15,43 @@ namespace CapstonePigLatin
             {
                 Console.WriteLine("Enter a line to be translated:");
                 string sentence = Console.ReadLine();
-                string PigLatin = GetSentenceInPigLatin(sentence);
+                string PigLatin = GetSentenceInPigLatin(sentence);//pulling method
                 Console.WriteLine(PigLatin);
                 Console.WriteLine("Continue? (y/n)");
                 answer = Console.ReadLine();
             } while (answer.Equals("y", StringComparison.InvariantCultureIgnoreCase));
         }
 
-        private static string GetSentenceInPigLatin(string sentence)
+        private static string GetSentenceInPigLatin(string sentence)//method
         {
             string vowel = "aeiou";
             
             {
                 StringBuilder sb = new StringBuilder();
-                StringBuilder consonant = new StringBuilder();
-                string lower = sentence.ToLower();
+                StringBuilder consonant = new StringBuilder();//stringbuilder for consonants
+                string lower = sentence.ToLower();//making sure it lower case
 
                 //seeing if the first letter is a vowel
                 if (vowel.Contains(lower[0].ToString()))
                 {
-                    sb.Append(lower);
-                    sb.Append("way");
+                    sb.Append(lower);//everything is lower case
+                    sb.Append("way");//moving way to the end of the word if a vowel if the first letter
                     return sb.ToString();
                 }
                 else
                 {
                     for (int i = 0; i < lower.Length; i++)
                     {
-                        if (!vowel.Contains(lower[i].ToString()))
+                        if (!vowel.Contains(lower[i].ToString()))//check if the first letter doesn't have a vowel
                         {
-                            consonant.Append(lower[i]);
+                            consonant.Append(lower[i]);//checking for consonants until you get to a vowel
                         }
                         else
                         {
                             sb.Append(lower.Substring(i));
-                            sb.Append(consonant);
+                            sb.Append(consonant);//move the consonants to the end and add "ay"
                             sb.Append("ay");
-                            break;
+                            break;//stop loop
                         }
                     }
                     return sb.ToString();
