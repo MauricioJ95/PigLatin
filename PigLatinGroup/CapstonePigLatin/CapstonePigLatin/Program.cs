f9af3a490d69acfa2f39c23cdf6533db2c8dfee7
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-15 23:10:56 -0400

    Initial commit

---
+++ PigLatinGroup/CapstonePigLatin/CapstonePigLatin/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstonePigLatin
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Enter a line to be translated:");
+                string sentence = Console.ReadLine();
+                string PigLatin = GetSentenceInPigLatin(sentence);
+                Console.WriteLine(PigLatin);
+                Console.WriteLine("Continue? (y/n)");
+                answer = Console.ReadLine();
+            } while (answer.Equals("y", StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private static string GetSentenceInPigLatin(string sentence)
+        {
+            string vowel = "aeiou";
+            
+            {
+                StringBuilder sb = new StringBuilder();
+                StringBuilder consonant = new StringBuilder();
+                string lower = sentence.ToLower();
+
+                //seeing if the first letter is a vowel
+                if (vowel.Contains(lower[0].ToString()))
+                {
+                    sb.Append(lower);
+                    sb.Append("way");
+                    return sb.ToString();
+                }
+                else
+                {
+                    for (int i = 0; i < lower.Length; i++)
+                    {
+                        if (!vowel.Contains(lower[i].ToString()))
+                        {
+                            consonant.Append(lower[i]);
+                        }
+                        else
+                        {
+                            sb.Append(lower.Substring(i));
+                            sb.Append(consonant);
+                            sb.Append("ay");
+                            break;
+                        }
+                    }
+                    return sb.ToString();
+                }
+            }
+
+        }
+    }
+}
